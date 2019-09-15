302a4483d0638b5edcf7dd7edc8b0f2af32f5de3
InseokChoi <kr.chis@gmail.com>
2019-09-15 11:10:06 +0900

    Order 추가 , populate 쿼리추가

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,35 +1,140 @@
 const express = require('express');
 const route = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
+const Order = require('../models/order');
+
 
 route.get('/',(req, res, next) => {
-    res.status(200).json({
-        message : 'Orders were fetched'
+    Order.find()
+    .select("_id product quantity")
+    .populate("product","name")
+    .exec()
+    .then(docs =>{
+        res.status(200).json({
+            count : docs.length,
+            products : docs.map(doc =>{
+                return {
+                    product : doc.product,
+                    quantity : doc.quantity,
+                    _id : doc._id,
+                    request : {
+                        type : "GET",
+                        url : "http://localhost:3000/orders/" + doc._id
+                    }
+                }
+            })
+        });
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        });
     });
+
 });
 
 route.post('/',(req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    }
-    res.status(201).json({
-        message : 'Order was created',
-        order: order
+    
+
+    Product.findById(req.body.productId)
+    .select("_id name price")
+    .exec()
+    .then(product =>{
+        if(!product){
+            return res.status(400).json({
+                message : "Product not found"
+            });
+        }
+        const order = new Order({
+            _id : mongoose.Types.ObjectId(),
+            quantity : req.body.quantity,
+            product : req.body.productId
+        });
+        return order.save()
+    })
+    .then(result=>{
+            console.log(result);
+            res.status(201).json({
+                message : 'Create order successfully',
+                createProduct : {
+                    product : result.product,
+                    quantity : result.quantity,
+                    _id : result._id,
+                    request : {
+                        type:"GET",
+                        url : "http://localhost:3000/orders/" + result._id
+                    }
+
+                }
+            });
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
+
+    
 });
 
 route.get('/:orderId',(req, res, next)=>{
-    res.status(201).json({
-        message : 'Order was details',
-        orderId : req.params.orderId
+    Order.findById(req.params.orderId)
+    .select("_id product quantity")
+    .exec()
+    .then(doc =>{
+        console.log('From database ' + doc);
+        if (doc){
+            res.status(200).json({
+                product : doc.product,
+                quantity : doc.quantity,
+                _id : doc._id,
+                request : {
+                    type : "GET",
+                    description : "Get all orders",
+                    url : "http://localhost:3000/orders"
+
+                }
+            });
+        }else{
+            res.status(404).json({messtae: 'No valid entry found for provided ID'});
+        }
+
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        });
     });
+    
 });
 
 route.delete('/:orderId',(req, res, next)=>{
-    res.status(201).json({
-        message : 'Order deleted',
-        orderId : req.params.orderId
+    Order.remove({_id:req.params.orderId})
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message : "Order deleted.",
+            request :{
+                type :"GET",
+                url : "http://localhost:3000/orders/",
+                data : {
+                    product : "ID" , quantity : "Number"
+                }            
+            }
+        });
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
+
 });
 
 
--- api/routes/products.js
+++ api/routes/products.js
@@ -6,11 +6,26 @@ const Product = require('../models/product');
 
 route.get('/',(req, res, next) => {
     Product.find()
+    .select("_id price name")
     .exec()
     .then(docs => {
-        console.log(docs);
+        //console.log(docs);
+        const response = {
+            count : docs.length,
+            products : docs.map(doc =>{
+                return {
+                    name : doc.name,
+                    price : doc.price,
+                    _id : doc._id,
+                    request : {
+                        type : "GET",
+                        url : "http://localhost:3000/products/" + doc._id
+                    }
+                }
+            })
+        }
         //if (docs.length >= 0){
-            res.status(200).json(docs);
+            res.status(200).json(response);
         // } else {
         //     res.status(400).json({
         //         message: 'No entries found'
@@ -40,8 +55,17 @@ route.post('/',(req, res, next) => {
         .then(result=>{
             console.log(result);
             res.status(201).json({
-                message : 'Handling Post requests to /products',
-                createProduct : product
+                message : 'Create product successfully',
+                createProduct : {
+                    name : result.name,
+                    price : result.price,
+                    _id : product._id,
+                    request : {
+                        type:"GET",
+                        url : "http://localhost:3000/products/" + result._id
+                    }
+
+                }
             });
         })
         .catch(err =>{
@@ -57,11 +81,22 @@ route.post('/',(req, res, next) => {
 route.get('/:productId',(req, res, next)=>{
     const id = req.params.productId;
     Product.findById(id)
+    .select("_id name price")
     .exec()
     .then(doc =>{
         console.log('From database ' + doc);
         if (doc){
-            res.status(200).json(doc);
+            res.status(200).json({
+                name : doc.name,
+                price : doc.price,
+                _id : doc._id,
+                request : {
+                    type : "GET",
+                    description : "Get all products",
+                    url : "http://localhost:3000/products"
+
+                }
+            });
         }else{
             res.status(404).json({messtae: 'No valid entry found for provided ID'});
         }
@@ -77,9 +112,33 @@ route.get('/:productId',(req, res, next)=>{
 });
 
 route.patch('/:productId',(req, res, next)=>{
-    res.status(200).json({
-        message : 'Update product'
+    const id = req.params.productId;
+    
+    const updateOps = {};
+    for (const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+        console.log(ops.propName);
+        console.log(ops.value);
+    }
+    Product.update({_id : id},{$set : updateOps})
+    .exec()
+    .then(result =>{
+        console.log(result);
+        res.status(200).json({
+            message : "Product updated.",
+            request :{
+                type :"GET",
+                url : "http://localhost:3000/products/" + id               
+            }
+        })
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error : err
+        });
     });
+    
 });
 
 route.delete('/:productId',(req, res, next)=>{
@@ -87,7 +146,16 @@ route.delete('/:productId',(req, res, next)=>{
     Product.remove({_id:id})
     .exec()
     .then(result => {
-        res.status(200).json(result);
+        res.status(200).json({
+            message : "Product deleted.",
+            request :{
+                type :"GET",
+                url : "http://localhost:3000/products/",
+                data : {
+                    name : "String" , price : "Number"
+                }            
+            }
+        });
     })
     .catch(err=>{
         console.log(err);
